167371eaf17b72d46dbd27823e8305a206825925
Rui Ferreira <ruiandreferreira@gmail.com>
2010-11-30 22:49:19 +0100

    completed genericcontainer, added queue and stack

---
+++ last.fm/GenericContainer/GenericStack.cpp
@@ -0,0 +1,4 @@
+#include "GenericStack.h"
+
+// unfortunately, with most compilers, template classes functions must be in the .h
+// i'm still keeping and compiling this file to get compile errors before linking
